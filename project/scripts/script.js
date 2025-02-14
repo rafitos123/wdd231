fb709a87e4495d44de55fa33bcafe895e19c76b6
lolrafael123 <rafael.antunes2705@gmail.com>
2025-02-14 20:03:32 -0300

    changes

--- project/scripts/script.js
+++ project/scripts/script.js
@@ -43,3 +43,57 @@ const today = new Date();
 
 const year = document.querySelector("#currentyear");
 year.innerHTML = today.getFullYear();
+
+
+
+const linksURL = "https://rafitos123.github.io/wdd231/project/data/smartphones.json";
+const cards = document.querySelector('#directory');
+
+//show smartphones in card
+async function getSmartphones() {
+    try {
+        const response = await fetch(linksURL);
+        const data = await response.json();
+        console.table(data.smartphones);
+        displayLinks(data.smartphones); 
+    } catch (error) {
+        console.error("Erro ao buscar os smartphones:", error);
+    }
+}
+
+
+    const displayLinks = (smartphones) => {
+    cards.innerHTML = ""; 
+
+    smartphones.forEach(smartphone => {
+        const card = document.createElement("div");
+        card.classList.add("card");
+
+        const container = document.createElement("div");
+        container.classList.add("container");
+
+        const image = document.createElement('img');
+        const name = document.createElement("h2");
+        const price = document.createElement("p");
+
+        image.setAttribute('src', smartphone.image);
+        image.setAttribute('alt', smartphone.name);
+        image.setAttribute('loading', 'lazy');
+        image.setAttribute('width', '100');
+        image.setAttribute('height', '100');
+
+        name.textContent = smartphone.name;
+        price.textContent = `Price: ${smartphone.price}`;
+
+        container.appendChild(image);
+        container.appendChild(name);
+        container.appendChild(price);
+
+        card.appendChild(container);
+        cards.appendChild(card);
+    });
+}
+
+getSmartphones();
+
+//show smartphones features
